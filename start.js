3283053745245f0ef37786cb181041112524879e
Spyretoz <pipisra@gmail.com>
2023-06-19 17:05:20 +0300

    Adding New Tables

--- start.js
+++ start.js
@@ -36,15 +36,15 @@ client.connect();
 app.get('/home', (req, res) => {
 	res.render('home', { title: "Welcome to Skroutz" });
 	
-	// client.query('Select * from product', (err, result)=>{
+	client.query('Select * from product', (err, result)=>{
 		
-	// 	if(!err){
-	// 		//console.log(client)
-	// 		console.log(result.rows[0])
-	// 		//res.send(result);
-	// 	}
-	// });
-	// client.end;
+		if(!err){
+			//console.log(client)
+			console.log(result.rows[0])
+			//res.send(result);
+		}
+	});
+	client.end;
 });
 
 
