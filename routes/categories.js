a340e70a61c409408f0fce5cc2ccf80d261f5f00
Spyretoz <pipisra@gmail.com>
2024-04-06 15:57:26 +0300

    Update: - create new nav bar and footer - add search box - align everything in center

--- routes/categories.js
+++ routes/categories.js
@@ -22,18 +22,5 @@ router.get('/all', async (req, res) => {
 	}
 });
 
-router.get('/all', async (req, res) => {
-
-	try {
-		// Show allacategories from database
-		const response = await client.query('SELECT * FROM CATEGORIES');
-		res.json(response.rows);
-
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Internal Server Error');
-	}
-});
-
 
 module.exports = router;
