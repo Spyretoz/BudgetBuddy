b11e0c6a2ec8048ce59c6758db4a085e39e23eb5
Spyretoz <pipisra@gmail.com>
2024-08-28 22:29:44 +0300

    New change

--- routes/categories.js
+++ routes/categories.js
@@ -13,13 +13,48 @@ router.get('/all', async (req, res) => {
 
 	try {
 		// Show all categories from database
-		const response = await client.query('SELECT categoryname, IMAGEURL FROM CATEGORIES ORDER BY categoryid');
+		
+        const query = 'SELECT categoryname, IMAGEURL FROM CATEGORIES ORDER BY categoryid';
+		const response = await client.query(query);
+
 		res.json(response.rows);
 
 	} catch (error) {
 		console.error(error);
 		res.status(500).send('Internal Server Error');
 	}
+});
+
+
+router.get('/:categoryid', async (req, res) => {
+
+	try {
+		const categoryID = req.params.categoryid;
+		res.render('products', { title: categoryID });
+
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Internal Server Error');
+	}
+
+});
+
+
+router.get('/:categoryid/products', async (req, res) => {
+
+	try {
+		const categoryID = req.params.categoryid;
+        const query = 'SELECT PRODUCTS.productid, PRODUCTS.name, PRODUCTS.price, PRODUCTS.imageurl FROM PRODUCTS LEFT OUTER JOIN CATEGORIES ON CATEGORIES.categoryID = PRODUCTS.categoryID WHERE categoryname = $1';
+        const values = [categoryID];
+        const response = await client.query(query, values);
+
+		res.json(response.rows);
+
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Internal Server Error');
+	}
+
 });
 
 
--- start.js
+++ start.js
@@ -41,9 +41,6 @@ app.use('/contact', contact);
 const mail = require('./routes/mail.js');
 app.use('/send-email', mail);
 
-const products = require('./routes/products.js');
-app.use('/products', products);
-
 
 
 // Express Messages Middleware
