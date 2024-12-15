5390e867e4d709766e52bb559d3a2c7192665ed0
Spyretoz <pipisra@gmail.com>
2024-12-15 18:54:42 +0200

    Update: - User Controller

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+const User = require('../models/userModel');
+
+
+exports.getProfile = async (req, res) => {
+	try {
+		const email = req.session.user.email;	
+
+		const user = await User.findOne({ where: { email } });
+		if (!user) {
+			return res.status(401).send('User not logged in.');
+		}
+
+        res.render('profile', { title: "My Profile" } );
+	} catch (error) {
+		console.error('Login error:', error);
+		res.status(500).send('An error occurred.');
+	}
+};
+
+
+exports.getOrders = async (req, res) => {
+	try {
+		const email = req.session.user.email;	
+
+		const user = await User.findOne({ where: { email } });
+		if (!user) {
+			return res.status(401).send('User not logged in.');
+		}
+
+        res.render('orders', { title: "My Orders" } );
+	} catch (error) {
+		console.error('Login error:', error);
+		res.status(500).send('An error occurred.');
+	}
+};
+
+
+exports.reviews = async (req, res) => {
+	try {
+		const email = req.session.user.email;	
+
+		const user = await User.findOne({ where: { email } });
+		if (!user) {
+			return res.status(401).send('User not logged in.');
+		}
+	} catch (error) {
+		console.error('Login error:', error);
+		res.status(500).send('An error occurred.');
+	}
+};
